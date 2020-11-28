7c8cbc2cec6e78d7a39da0f0d34794fbf61a914a
old-luo <luodianlei7@163.com>
2020-11-28 16:39:38 +0800

    buffer

--- day01/08.Buffer缓冲器/buffer.js
+++ day01/08.Buffer缓冲器/buffer.js
@@ -6,3 +6,19 @@
  *     4.每个元素占用内存的空间是1字节。
  *     5.Buffer是Node中非常核心的模块，无需下载，无需引入，直接可以使用
  * */
+
+// console.log(Buffer)
+// 弃用了
+// const buf = new Buffer(10)
+// console.log(buf)
+
+// Buffer.alloc() 性能较高,也比较安全
+// const buf = Buffer.alloc(10)
+// console.log(buf)
+// Buffer.allocUnsafe() 性能最高,但是不安全
+// const buf = Buffer.allocUnsafe(10)
+// console.log(buf)
+
+// 直接把一个数据转成buffer
+const buf = Buffer.from('hello nodexxx')
+console.log(buf)
