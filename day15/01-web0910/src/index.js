b8608d398d7fdf97544d08aced7724a943523d4f
old-luo <luodianlei7@163.com>
2020-12-18 16:46:15 +0800

    redux介绍

--- day15/01-web0910/src/index.js
+++ day15/01-web0910/src/index.js
@@ -1,6 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 
-import App from "./App";
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// 演示redux的使用
+// 3. 创建reducer
+// 注意: 定义reducer的时候,给state设置默认值就是给仓库设置初始数据
+const fn = (state = { count: 0, msg: '123' }, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+// 1. 引入createStore函数 import {createStore} from 'redux'
+// 2. 创建store对象
+const store = createStore(fn)
+
+console.log(store)
+console.log(store.getState())
