0152935fb438b1319436ec203c6dc958369bbc77
old-luo <luodianlei7@163.com>
2020-12-25 11:49:55 +0800

    验证验证码

--- day20/react-project0910/src/api/register.js
+++ day20/react-project0910/src/api/register.js
@@ -17,5 +17,20 @@ function verifyPhone(phone) {
     }
   })
 }
+function verifyCode(phone, code) {
+  //发送异步请求
+  // 注意: 默认情况会跨域
+  // 解决跨域的方式: 使用代理:
+  // 1. package.json中增加配置量: "proxy":"http://localhost:5000"
+  // 2. 发请求时,不要写绝对路径了
+  return axios({
+    url: '/regist/verify_code',
+    method: 'post',
+    data: {
+      phone,
+      code
+    }
+  })
+}
 
-export { verifyPhone }
+export { verifyPhone, verifyCode }
--- day20/react-project0910/src/pages/Register/RegisterCode/index.js
+++ day20/react-project0910/src/pages/Register/RegisterCode/index.js
@@ -6,27 +6,86 @@ import {
   WingBlank,
   WhiteSpace,
   Button,
-  Modal
+  Modal,
+  Toast
 } from 'antd-mobile'
+import { createForm } from 'rc-form'
 
 import { sendCode } from '../../../api/login'
 import './index.css'
 import msg from './msg.png'
-export default class RegisterCode extends Component {
-  async componentDidMount() {
+class RegisterCode extends Component {
+  state = {
+    // 倒计时的时间
+    downcount: 5,
+    isDisabled: true
+  }
+  componentDidMount() {
     // 直接给后台发送请求,让后台给用户发送验证码
     // 从本地缓存中获取电话号
+    this.sendCodeFn()
+  }
+  //封装发送验证码的函数
+  sendCodeFn = async () => {
     const phone = localStorage.getItem('phone')
     const res = await sendCode(phone)
-    console.log(res)
+    if (res.data.success) {
+      Toast.success('验证码发送成功', 1)
+
+      //倒计时
+      this.timeid = setInterval(() => {
+        if (this.state.downcount === 1) {
+          clearInterval(this.timeid)
+          this.setState({
+            downcount: 5
+          })
+          return
+        }
+        this.setState({
+          downcount: this.state.downcount - 1
+        })
+      }, 1000)
+    }
   }
+
+  // 发送验证码的函数
+  sendVerifyCode = () => {
+    if (this.state.downcount === 5) {
+      //发送请求,发送验验证码
+      // console.log(1111111)
+      this.sendCodeFn()
+    }
+  }
+
+  // 校验验证码
+  validator = (rules, value, cb) => {
+    // console.log(rules)
+    // console.log(value)
+    // 判断用户输入的内容是否是手机号
+    if (/^\d{6}$/.test(value)) {
+      //是手机号
+      // console.log('是手机号')
+      this.setState({
+        isDisabled: false
+      })
+    } else {
+      //不是手机号
+      this.setState({
+        isDisabled: true
+      })
+    }
+  }
+
+  // 发送请求验证验证码是否正确
+  handleVerifyCode = () => {}
   render() {
+    const { getFieldProps } = this.props.form
     return (
       <div className='wrap'>
         <NavBar
           mode='light'
           icon={<Icon type='left' color='#000' />}
-          onLeftClick={console.log(111)}
+          onLeftClick={() => {}}
         >
           硅谷注册
         </NavBar>
@@ -45,11 +104,35 @@ export default class RegisterCode extends Component {
           <WhiteSpace size='lg'></WhiteSpace>
 
           <div className='code'>
-            <InputItem clear placeholder='请输入手机验证码'></InputItem>
-            <button className='code-btn'>重新发送(40s)</button>
+            <InputItem
+              {...getFieldProps('code', {
+                rules: [
+                  {
+                    validator: this.validator
+                  }
+                ]
+              })}
+              clear
+              placeholder='请输入手机验证码'
+            ></InputItem>
+            <button
+              className={
+                this.state.downcount === 5 ? 'code-btn active' : 'code-btn'
+              }
+              onClick={this.sendVerifyCode}
+            >
+              {this.state.downcount === 5
+                ? '获取验证码'
+                : `重新发送(${this.state.downcount}s)`}
+            </button>
           </div>
           <WingBlank size='lg'>
-            <Button className='code-next' type='warning' disabled={true}>
+            <Button
+              className='code-next'
+              type='warning'
+              disabled={this.state.isDisabled}
+              onClick={this.handleVerifyCode}
+            >
               下一步
             </Button>
           </WingBlank>
@@ -62,3 +145,5 @@ export default class RegisterCode extends Component {
     )
   }
 }
+
+export default createForm()(RegisterCode)
