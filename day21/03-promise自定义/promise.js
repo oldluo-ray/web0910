639495eeda939483dd0b2ebf4b96947b53ac6a7c
old-luo <luodianlei7@163.com>
2020-12-26 14:43:40 +0800

    promisethen函数01

--- day21/03-promise自定义/promise.js
+++ day21/03-promise自定义/promise.js
@@ -13,6 +13,7 @@
     // 定义promise实例的状态和结果
     this.state = PENDING
     this.result = undefined
+    this.container = [] //用来存储成功和失败的回调函数
 
     //当resolve函数被调用的时候,要做的 事情就是改变状态以及修改结果
     const resolve = value => {
@@ -20,6 +21,12 @@
       if (this.state !== PENDING) return
       this.state = RESOLVED
       this.result = value
+
+      //成功了,在这里调用then函数中的第一个参数(onResolved)
+      // 注意: 因为有可能then被调用多次,所以遍历数组,从数组中,拿到所有的成功,然后调用
+      this.container.forEach(item => {
+        item.onResolved(this.result)
+      })
     }
 
     // reject中将状态改成失败
@@ -27,9 +34,27 @@
       if (this.state !== PENDING) return
       this.state = REJECTED
       this.result = error
+      // 失败了,在这里调用then函数的第二个参数(onRejected)
+      this.container.forEach(item => {
+        item.onRejected(this.result)
+      })
     }
 
     // executor是promise的执行器,在new promise的时候,立即调用(同步回调)
     executor(resolve, reject)
+  }
+
+  // then函数,添加到原型上
+  window.Promise.prototype.then = function(onResolved, onRejected) {
+    // 如果执行到then函数的时候,状态已经变了,就立刻执行成功和失败的回调
+    if (this.state === RESOLVED) {
+      onResolved(this.result)
+    } else if (this.state === REJECTED) {
+      onRejected(this.result)
+    } else {
+      // 如果执行then的时候,promise状态还是pending,就把
+      // onResolved, onRejected添加到promise实例身上
+      this.container.push({ onResolved, onRejected })
+    }
   }
 })()
